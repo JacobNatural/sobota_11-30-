6e7050ca8709fc6ca874ef96b22cbc9eb49a406b
JacobNatural <jakubmarjankowski@gmail.com>
2024-02-24 15:15:38 +0100

    new comit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
         System.out.println("modyfikacjq");
+        System.out.println("Zmiana");
 
     }
 }
